43e55dea7ca3bc2749f2b0137f207395eb328d08
Brokenmoon2 <1941658307@qq.com>
2024-11-23 21:49:46 +0800

    评论点赞与评论删除

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,6 @@
 package utils
 
+// InList 判断key是否存在与列表中
 func InList(key string, list []string) bool {
 	for _, s := range list {
 		if key == s {
@@ -7,4 +8,11 @@ func InList(key string, list []string) bool {
 		}
 	}
 	return false
+}
+
+// Reverse 切片反转
+func Reverse[T any](slice []T) {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
 }
