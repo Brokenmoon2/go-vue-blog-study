45051da5abd24a1666e329314b539daa063196ee
Brokenmoon2 <1941658307@qq.com>
2024-11-26 15:39:53 +0800

    聊天记录

--- routers/chat_router.go
+++ routers/chat_router.go
@@ -5,4 +5,5 @@ import "go-vue-blog-study/api"
 func (router RouterGroup) ChatRouter() {
 	app := api.ApiGroupApp.ChatApi
 	router.GET("chat_groups", app.ChatGroupView)
+	router.GET("chat_groups_records", app.ChatListView)
 }
