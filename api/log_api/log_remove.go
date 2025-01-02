c1c497c2bff327a49e94af4e3ccc211a73553408
Brokenmoon2 <1941658307@qq.com>
2025-01-02 10:34:16 +0800

    日志删除

---
+++ api/log_api/log_remove.go
@@ -0,0 +1,29 @@
+package log_api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go-vue-blog-study/global"
+	"go-vue-blog-study/models"
+	"go-vue-blog-study/models/res"
+	"go-vue-blog-study/plugins/log_stash"
+)
+
+func (LogApi) LogRemoveListView(c *gin.Context) {
+	var cr models.RemoveRequest
+	err := c.ShouldBindJSON(&cr)
+	if err != nil {
+		res.FailWithCode(res.ArgumentError, c)
+		return
+	}
+
+	var list []log_stash.LogStashModel
+	count := global.DB.Find(&list, cr.IDList).RowsAffected
+	if count == 0 {
+		res.FailWithMessage("日志不存在", c)
+		return
+	}
+	global.DB.Delete(&list)
+	res.OkWithMessage(fmt.Sprintf("共删除 %d 个日志", count), c)
+
+}
--- routers/log_router.go
+++ routers/log_router.go
@@ -1,8 +1,12 @@
 package routers
 
-import "go-vue-blog-study/api"
+import (
+	"go-vue-blog-study/api"
+	"go-vue-blog-study/middleware"
+)
 
 func (router RouterGroup) LogRouter() {
 	app := api.ApiGroupApp.LogApi
 	router.GET("logs", app.LogListView)
+	router.DELETE("logs", middleware.JwtAdmin(), app.LogRemoveListView)
 }
