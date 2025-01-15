16d48eaa71be0acbfe12e83b7d4c2f9dae92bd88
Brokenmoon2 <1941658307@qq.com>
2025-01-15 10:36:35 +0800

    首页完成

--- api/tag_api/tag_update.go
+++ api/tag_api/tag_update.go
@@ -8,6 +8,16 @@ import (
 	"go-vue-blog-study/models/res"
 )
 
+// TagUpdateView 更新标签
+// @Tags 标签管理
+// @Summary 更新标签
+// @Description 更新标签
+// @Param data body TagRequest  true "查询参数"
+// @Param token header string  true  "token"
+// @Param id path int  true  "id"
+// @Router /api/tags/{id} [put]
+// @Produce json
+// @Success 200 {object} res.Response{}
 func (TagApi) TagUpdateView(c *gin.Context) {
 
 	id := c.Param("id")
--- routers/tag_router.go
+++ routers/tag_router.go
@@ -1,12 +1,15 @@
 package routers
 
-import "go-vue-blog-study/api"
+import (
+	"go-vue-blog-study/api"
+	"go-vue-blog-study/middleware"
+)
 
 func (router RouterGroup) TagRouter() {
 	app := api.ApiGroupApp.TagApi
-	router.POST("tags", app.TagCreateView)
+	router.POST("tags", middleware.JwtAdmin(), app.TagCreateView)
 	router.GET("tags", app.TagListView)
 	router.GET("tag_names", app.TagNameListView)
-	router.PUT("tags/:id", app.TagUpdateView)
-	router.DELETE("tags", app.TagRemoveView)
+	router.PUT("tags/:id", middleware.JwtAdmin(), app.TagUpdateView)
+	router.DELETE("tags", middleware.JwtAdmin(), app.TagRemoveView)
 }
